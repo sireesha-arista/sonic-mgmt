3d86d722016160a893a486edbc4f37a3ef80fea1
Sireesha Lingareddy <sireesha-ext@arista.com>
2026-04-21 11:08:48 +0530

    [202511][sonic-mgmt]: Ignore expected cleanup ERR in testFdbMacLearning
    prepare_test - shuts all interfaces before the test
    runs. Orchagent processes link-down events asynchronously, triggering
    mux tunnel route cleanup that spills 3 seconds past the LogAnalyzer
    start marker, causing false teardown failures. The errors
    (meta_sai_validate_route_entry: doesn't exist) are harmless double-
    delete race artifacts unrelated to FDB MAC learning. Add the pattern
    to ignore_expected_loganalyzer_exception to suppress the false positive.
    
    Signed-off-by: Sireesha Lingareddy <sireesha-ext@arista.com>

--- tests/fdb/test_fdb_mac_learning.py
+++ tests/fdb/test_fdb_mac_learning.py
@@ -22,7 +22,9 @@ def ignore_expected_loganalyzer_exception(loganalyzer, duthosts):
 
     ignore_errors = [
         r".*ERR swss#orchagent: .*update: Failed to get port by bridge port ID.*",
-        r".* ERR swss#tunnel_packet_handler.py: All portchannels failed to come up within \d+ minutes, exiting.*"
+        r".* ERR swss#tunnel_packet_handler.py: All portchannels failed to come up within \d+ minutes, exiting.*",
+        r".*ERR swss#orchagent: .*meta_sai_validate_route_entry: "
+        r"object key SAI_OBJECT_TYPE_ROUTE_ENTRY:.* doesn't exist.*",
         ]
     if loganalyzer:
         for duthost in duthosts:
